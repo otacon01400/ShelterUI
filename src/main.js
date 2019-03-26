f06daebf826de97a7f663033e46812e0dbe053d8
Daniel Nicolás <danielnicolastvjb12013@cev.com>
2019-03-26 11:12:03 +0100

    Remove Apollo. Conflict resolved

--- src/main.js
+++ src/main.js
@@ -3,12 +3,10 @@ import './plugins/vuetify'
 import './plugins/vuetify'
 import App from './App.vue'
 import router from './router'
-import { createProvider } from './vue-apollo'
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
-  apolloProvider: createProvider(),
   render: h => h(App)
 }).$mount('#app')
